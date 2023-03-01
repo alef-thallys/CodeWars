6965564ea6f0c36355422d2c6094ee36e5243ec5
Alef Thallys <122895844+alef-thallys@users.noreply.github.com>
2023-03-01 19:26:44 -0300

    Update script.js

--- Reversewords-7kyu/script.js
+++ Reversewords-7kyu/script.js
@@ -1,7 +1,4 @@
 function reverseWords(str) {
-  return str
-    .split(" ")
-    .map((word) => word.split("").reverse().join(""))
-    .join(" ");
+  return str.split(" ").map((word) => word.split("").reverse().join("")).join(" ");
 }
-console.log(reverseWords("The quick brown fox jumps over the lazy dog."));
+

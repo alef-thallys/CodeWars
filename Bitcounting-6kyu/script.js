86a9d45c48d0475cfe95cdd06292dbfa8ed99362
alef-thallys <alef.thallys15@hotmail.com>
2023-03-03 23:10:35 -0300

    add-kata

---
+++ Bitcounting-6kyu/script.js
@@ -0,0 +1,13 @@
+function countBits(n) {
+  let arr = [];
+  let sum = 0;
+  while (n > 0) {
+    n % 2 === 0 ? arr.push(0) : arr.push(1);
+    n = parseInt(n / 2);
+  }
+  arr.reverse().forEach((e) => {
+    e === 1 ? sum++ : false;
+  });
+  return sum;
+}
+console.log(countBits(1234));
